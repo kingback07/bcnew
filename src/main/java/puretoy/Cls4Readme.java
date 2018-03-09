b98f4167db152c2c81ae4108e8398d8d87724632
kingback07 <kingback_07@126.com>
2018-03-09 11:23:23 +0800

    8th commit for the branch dev

--- src/main/java/puretoy/Cls4Readme.java
+++ src/main/java/puretoy/Cls4Readme.java
@@ -2,4 +2,14 @@ package puretoy;
 
 public class Cls4Readme {
 
+	private int testInt;
+
+	public int getTestInt() {
+		return testInt;
+	}
+
+	public void setTestInt(int testInt) {
+		this.testInt = testInt;
+	}
+	
 }
