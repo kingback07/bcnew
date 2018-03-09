519b7e7443058aae043ef24727d587fdc7aadf19
kingback07 <kingback_07@126.com>
2018-03-09 11:25:32 +0800

    9th commit for the master on Cls4Readme

--- src/main/java/puretoy/Cls4Readme.java
+++ src/main/java/puretoy/Cls4Readme.java
@@ -2,4 +2,14 @@ package puretoy;
 
 public class Cls4Readme {
 
+	private String readName;
+
+	public String getReadName() {
+		return readName;
+	}
+
+	public void setReadName(String readName) {
+		this.readName = readName;
+	}
+	
 }
